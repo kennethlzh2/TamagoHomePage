6ea3550e0c6a13d65904d38bca7f5c24b43f5301
Kenneth Lee <kennethlee@kenneths-macbook-pro.local>
2018-06-08 18:24:50 +0800

    Initial Commit

---
+++ TamagoHomePage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TamagoHomePage
+//
+//  Created by Kenneth Lee on 08/06/2018.
+//  Copyright © 2018 Kenneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TamagoHomePage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TamagoHomePage
+//
+//  Created by Kenneth Lee on 08/06/2018.
+//  Copyright © 2018 Kenneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
