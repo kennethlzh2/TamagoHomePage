643a3494662bfd778819d6eeb9700f89687477c2
Kenneth Lee <kennethlee@kenneths-macbook-pro.local>
2018-06-09 01:08:16 +0800

    Updated

---
+++ TamagoHomePage/View/NavigationBarCustomView.h
@@ -0,0 +1,16 @@
+//
+//  NavigationBarCustomView.h
+//  TamagoHomePage
+//
+//  Created by Kenneth Lee on 09/06/2018.
+//  Copyright © 2018 Kenneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationBarCustomView : UIView
+@property (weak, nonatomic) IBOutlet UIView *channelsView;
+@property (weak, nonatomic) IBOutlet UIView *tamagoView;
+@property (weak, nonatomic) IBOutlet UIView *discoveryView;
+
+@end
---
+++ TamagoHomePage/View/NavigationBarCustomView.m
@@ -0,0 +1,21 @@
+//
+//  NavigationBarCustomView.m
+//  TamagoHomePage
+//
+//  Created by Kenneth Lee on 09/06/2018.
+//  Copyright © 2018 Kenneth. All rights reserved.
+//
+
+#import "NavigationBarCustomView.h"
+
+@implementation NavigationBarCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
